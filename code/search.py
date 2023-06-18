7d5302d8bf76f57761deb18686b01bfd8a63986a
osullik <osullik@protonmail.com>
2023-06-18 19:10:47 +0400

    integrated inverted_index.ipynb to search.py and updated the search script to call it multiple times.

--- code/search.py
+++ code/search.py
@@ -1,37 +1,90 @@
 # Code for Search in Gestalt
 
 import pandas as pd
-import glob
-import os
-from prompt_toolkit import prompt
-from prompt_toolkit.completion import Completer, Completion
+from collections import OrderedDict
+
+def normalize_object_term(term):
+    # TODO implement fuzzy term matching with OSM API
+    return term
 
 class InvertedIndex:
     """
     """
     def __init__(self, filename):
-        self.objects_df = pd.read_csv(filename)[['name','predicted_location']]
+        self.df = pd.read_csv(filename)
+        self.objects_df = self.df[['name','predicted_location']]
         self.objects_df_grouped = self.objects_df.groupby(self.objects_df.columns.tolist(),as_index=False).size()
-        self.objects_df_grouped = self.objects_df_grouped.rename(columns={'size':'num_occurences'})
+        self.objects_df_grouped = self.objects_df_grouped.rename(columns={'size':'num_occurences'})  # this puts object counts in there, but we drop it for now
         self.objects_df_grouped_list = pd.DataFrame(self.objects_df_grouped.groupby('name')['predicted_location'].apply(list))
         self.__make_ii__()
+        self.__make_adj__()
+
     def __make_ii__(self):
         self.ii = dict()
+        self.ii_counter = dict()
         for idx, row in self.objects_df_grouped_list.iterrows():
             self.ii[idx] = set(row['predicted_location'])
-    
-    def search(self, arg):                      #if want to use a string list of input terms change line to: def search(self, *arg):
-        set_list = [self.ii[x] for x in arg]
+            self.ii_counter[idx] = len(set(row['predicted_location']))
+
+    def most_discriminative(self, query_terms):
+        best_qt = query_terms[0]
+        for qt in query_terms:
+            if self.ii_counter[qt] < self.ii_counter[best_qt]:
+                best_qt = qt
+        return best_qt
+
+    def __make_adj__(self):
+        self.df['prob'] = self.df['object_prob'] * self.df['assignment_prob']
+        self.adj = self.df.groupby(['name','predicted_location'], as_index=False).agg({'prob':'max'})
+        return self.adj
+
+
+    def get_prob(self, Loc : str, Obj : str):
+        return self.adj[(self.adj['name'] == Obj) & (self.adj['predicted_location'] == Loc)]['prob'].item()
+
+
+    def rank(self, Locs : set, Objs : set):
+        loc_ranks = OrderedDict()
+        for L in Locs:
+            p = 1
+            for Ob in Objs:
+                p *= self.get_prob(L, Ob)
+            loc_ranks[L] = p
+        return sorted(loc_ranks, key=loc_ranks.get, reverse=True)
+
+    def __search__(self, query_terms : list):
+        set_list = [self.ii[normalize_object_term(x)] for x in query_terms]
         return set.intersection(*set_list)
 
-#class SearchCompleter(Completer):
-#    def __init__(self, inverted_index: InvertedIndex):
-#        self.inverted_index = inverted_index
-#    def get_completions(self, document, complete_event):
-#        if complete_event.completion_requested:
-#            for match in self.inverted_index.search(document.text):
-#                yield Completion(match.ljust(document.cursor_position), start_position=-document.cursor_position)
-                
+    def search(self, query_terms : list):
+        set_list = [self.ii[normalize_object_term(x)] for x in query_terms]
+        return list(set.intersection(*set_list)), query_terms
+
+    def ranked_search(self, query_terms : list):
+        set_list = [self.ii[normalize_object_term(x)] for x in query_terms]
+        return self.rank(set.intersection(*set_list), query_terms), query_terms
+
+    def fuzzy_search(self, query_terms : list):
+        # Assume inverted index of object_class -> {Locations} exists
+        s = self.__search__(query_terms)
+        if len(s) == 0:
+            q = []
+            while True:  # TODO make this cleaner and check end conditions
+                best_qt = self.most_discriminative(query_terms)
+                q.append(best_qt)
+                query_terms.remove(best_qt)
+                s = self.__search__(q)
+                if len(s) > 0:
+                    continue
+                else:  # went too far, adding one too many search terms
+                    q.pop()  # Remove most recent
+                    s = self.__search__(q)  # Rerun to get nonempty result
+                    query_terms = q
+                    break
+        if len(s) > 1:
+            self.rank(s, query_terms)
+        return s, query_terms  # returns query_terms since they may be a reduces subset of original query terms
+     
                 
 #def main():
 #    directory_name = os.getcwd()
--- scripts/70_searhGestalt.sh
+++ scripts/70_searhGestalt.sh
@@ -5,11 +5,18 @@
 #Variables (update these to change the python script that runs)
     #Search Terms is a string with spaces delimiting search terms
 INPUT_FILE="../data/output/ownershipAssignment/DBSCAN_PredictedLocations.csv"
-SEARCH_TERMS="tree bird"
+SEARCH_TERMS1="apple wine_barrell"
+SEARCH_TERMS2="fork bench bird"
+SEARCH_TERMS3="bench bird"
+SEARCH_TERMS4="bird"
+
 
 
 #Activate the venv
 source ../gestalt_env/bin/activate
 
 #Run the script
-python3 ../code/gestalt.py --gestaltSearch --inputFile $INPUT_FILE --searchterms $SEARCH_TERMS
+python3 ../code/gestalt.py --gestaltSearch --inputFile $INPUT_FILE --searchterms $SEARCH_TERMS1
+python3 ../code/gestalt.py --gestaltSearch --inputFile $INPUT_FILE --searchterms $SEARCH_TERMS2
+python3 ../code/gestalt.py --gestaltSearch --inputFile $INPUT_FILE --searchterms $SEARCH_TERMS3
+python3 ../code/gestalt.py --gestaltSearch --inputFile $INPUT_FILE --searchterms $SEARCH_TERMS4
