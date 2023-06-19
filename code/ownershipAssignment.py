3e89292364f101ff453a7505c9183b8c2a7bfc32
osullik <osullik@protonmail.com>
2023-06-19 14:08:21 +1000

    Fixed tabbing errors

--- code/ownershipAssignment.py
+++ code/ownershipAssignment.py
@@ -227,31 +227,31 @@ class OwnershipAssigner():
 			dists.append(self.__distance__(obj_coord, centroid_coord))
    
 		self._df_objects['assignment_prob'] = 1 - self.__normalize_probs__(dists, mask=list(self._df_objects['cluster'] != -1))
-		display(self._df_objects)
+		#display(self._df_objects)
   
 		# Fuzzy multiple asn
 		if fuzzy_threshold > 0:
-				df_multi_asn_objects = self._df_objects.copy()
-				for idx, row in self._df_objects.iterrows():
-						for centroid in centroids:
-								obj_centroid_dist = self.__distance__((row['latitude'], row['longitude']), centroid)
-								# if obj-centroid distance is within THRESHOLD% of range of obj-centroid distances we saw during exact assignment  
-								threshold = fuzzy_threshold * (self.cluster_max_dist - self.cluster_min_dist)
-								print("THRESHOLD : ", threshold)
-								print("range : ", self.cluster_max_dist - self.cluster_min_dist)              
-								print("ACTIAL: ", obj_centroid_dist - self.cluster_min_dist)                       
-								if (obj_centroid_dist - self.cluster_min_dist) < threshold:
-										#print("doubling up on: ", idx)          
-										df_multi_asn_objects = df_multi_asn_objects.append(row)
-										#display(pd.concat([df_multi_asn_objects, pd.DataFrame(row, index=[idx], columns=df_multi_asn_objects.columns)], axis=1, ignore_index=True).tail())
-								
+			df_multi_asn_objects = self._df_objects.copy()
+			for idx, row in self._df_objects.iterrows():
+				for centroid in centroids:
+					obj_centroid_dist = self.__distance__((row['latitude'], row['longitude']), centroid)
+					# if obj-centroid distance is within THRESHOLD% of range of obj-centroid distances we saw during exact assignment  
+					threshold = fuzzy_threshold * (self.cluster_max_dist - self.cluster_min_dist)
+					print("THRESHOLD : ", threshold)
+					print("range : ", self.cluster_max_dist - self.cluster_min_dist)              
+					print("ACTIAL: ", obj_centroid_dist - self.cluster_min_dist)                       
+					if (obj_centroid_dist - self.cluster_min_dist) < threshold:
+							#print("doubling up on: ", idx)          
+							df_multi_asn_objects = df_multi_asn_objects.append(row)
+							#display(pd.concat([df_multi_asn_objects, pd.DataFrame(row, index=[idx], columns=df_multi_asn_objects.columns)], axis=1, ignore_index=True).tail())
+					
 		
-		self._df_objects = df_multi_asn_objects  
+			self._df_objects = df_multi_asn_objects  
 
-		self.inferLocation(self._df_objects, centroids,"dbscan")
+			self.inferLocation(self._df_objects, centroids,"dbscan")
   
-		self._df_objects = df_multi_asn_objects 
-		display(self._df_objects)
+			self._df_objects = df_multi_asn_objects 
+		#display(self._df_objects)
 		
 		
 	def __distance__(self, point1, point2):
