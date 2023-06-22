03ea6711aaecb4a0bce6c260c9b50efee4f6097d
osullik <osullik@protonmail.com>
2023-06-23 01:23:43 +1000

    Updates to get e2e execution working

--- scripts/60b_assign_dbscan.sh
+++ scripts/60b_assign_dbscan.sh
@@ -5,12 +5,13 @@
 #Variables (update these to change the python script that runs)
 INPUT_DIRECTORY="../data/output/datacollection"
 OUTPUT_DIRECTORY="../data/output/ownershipAssignment"
-# Epsilon here = 0.1/6371 (i.e. approx 180m in lat/long)
-EPSILON=0.000015696123058
+# Epsilon here = 0.05/6371 (i.e. approx 180m in lat/long)
+EPSILON=0.00001569612
 MIN_CLUSTER_SIZE=3
+FUZZY_THRESHOLD=0.0
 
 #Activate the venv
 source ../gestalt_env/bin/activate
 
 #Run the script
-python3 ../code/gestalt.py --ownershipAssignment dbscan --inputDirectory $INPUT_DIRECTORY --outputDirectory $OUTPUT_DIRECTORY --epsilon $EPSILON --numClusters $MIN_CLUSTER_SIZE
+python3 ../code/gestalt.py --ownershipAssignment dbscan --inputDirectory $INPUT_DIRECTORY --outputDirectory $OUTPUT_DIRECTORY --epsilon $EPSILON --numClusters $MIN_CLUSTER_SIZE --fuzzy_threshold $FUZZY_THRESHOLD
--- scripts/61_createConceptMaps.sh
+++ scripts/61_createConceptMaps.sh
@@ -5,9 +5,10 @@
 #Variables (update these to change the python script that runs)
 INPUT_FILE="../data/output/ownershipAssignment/DBSCAN_PredictedLocations.csv"
 OUTPUT_DIRECTORY="../data/output/concept_mapping"
+LOCATIONS_FILE="../data/output/dataCollection/locations_-31.90009882641578115.96168231510637-31.77307863942101116.05029961853784_alllocations.json"
 
 #Activate the venv
 source ../gestalt_env/bin/activate
 
 #Run the script
-python3 ../code/gestalt.py -ccm --inputFile $INPUT_FILE --outputDirectory $OUTPUT_DIRECTORY
+python3 ../code/gestalt.py -ccm --inputFile $INPUT_FILE --outputDirectory $OUTPUT_DIRECTORY --fileSource $LOCATIONS_FILE
