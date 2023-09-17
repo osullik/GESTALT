484d1521385e79e565035b6c69d251286b67e513
osullik <osullik@protonmail.com>
2023-09-16 22:19:11 -0400

    Saving output to file

--- code/utils/dataGenerator.py
+++ code/utils/dataGenerator.py
@@ -5,10 +5,13 @@
 import sys
 import os
 import unittest
+import random
+import string 
 # Library Imports
 import pandas as pd
 import numpy as np
 import networkit as nk
+import matplotlib.pyplot as plt
 
 # User file imports
 
@@ -17,8 +20,7 @@ sys.path.insert(1, os.getcwd()+"/../compass")
 sys.path.insert(1, os.getcwd()+"/../gestalt")
 sys.path.insert(1, os.getcwd()+"/../experiments")
 
-from compass import Point
-from compass import Compass
+from compass import Point, Compass
 from conceptMapping import ConceptMapper
 from search import InvertedIndex
 from exp_compass import CompassExperimentRunner, CompassDataLoader
@@ -27,25 +29,101 @@ class DataGenerator():
     def __init__(self):
         pass
 
-    def generateMatrix(self,locationSize:int,numObjects:int,labels:list[str]):
+    def generateMatrix(self,scaleFactor:int,edgeFactor:int):
+        points = []
 
-        rmat = nk.generators.RmatGenerator(locationSize, 1, 0.1, 0.2, 0.5, 0.2)
-
+        rmat = nk.generators.RmatGenerator(scaleFactor, edgeFactor, 0.57, 0.19, 0.19, 0.05) #Parameters taken from Graph500
         rmatG = rmat.generate()
 
-        print(rmatG)
+        #print(nk.algebraic.adjacencyMatrix(rmatG,matrixType='sparse'))
+        #print(nk.algebraic.adjacencyMatrix(rmatG,matrixType='dense'))
 
-        print(nk.algebraic.adjacencyMatrix(rmatG,matrixType='sparse'))
-        print(nk.algebraic.adjacencyMatrix(rmatG,matrixType='dense'))
-
+        #Generate a sparse matrix of the RMAT generated material. 
         sm = nk.algebraic.adjacencyMatrix(rmatG,matrixType='sparse')
-            
-        row,col = sm.nonzero()
 
-        points = []
+        #Extract row and column info from the sparse matrix
+        row,col = sm.nonzero()  
             
         for r,c in zip(row,col):
             points.append((r,c))
 
-
         return(points)
+
+    def labelNodes(self,points:list[tuple], numClasses:int, random_seed:int)->dict:
+
+        RANDOM_POWER = random_seed  #the higher the random_seed value, the more skewed the power distribution
+        SAMPLES = 1000
+
+        np.random.seed(random_seed)
+        random.seed(random_seed)
+
+        namedPoints = {}
+        classes = []
+        labels = []
+    
+        #Randomly choose labels to be in the labelList
+        if numClasses > len(string.ascii_letters):
+            print("Maximum number of classes is", len(string.ascii_letters), "setting numClasses to:", len(string.ascii_letters))
+        while len(classes) < numClasses:
+            classes.append(random.choice(string.ascii_letters))
+        
+        #Create a sample power-law distribution, and divide it into bins representing each possible class
+        s = np.random.power(RANDOM_POWER, SAMPLES)
+        count, bins, ignored = plt.hist(s, bins=numClasses)
+        binList = np.digitize(s,bins)
+
+        names = []
+        longitudes = []
+        latitudes = []
+
+        #For each coordinate, assign it a label based on the power law distribution we generated. 
+        for i in range(0, len(points)):
+            coord = points[i]
+            labelIndex = binList[i % len(points)]  #Start at beginning if longer than list
+            label = classes[labelIndex-1]
+            names.append(label)
+            longitudes.append(coord[0])
+            latitudes.append(coord[1])
+
+        #Build the dictionary.
+        namedPoints["name"] = names
+        namedPoints["longitude"] = longitudes
+        namedPoints["latitude"] = latitudes
+
+        return(namedPoints)
+
+    def saveToFile(self, experiment_name:str, saveType:str, number:int, data:dict):
+
+        dataDirectory = ""
+        for p in sys.path:
+            if p.endswith("data"):
+                dataDirectory = p
+        
+        assert((dataDirectory in sys.path),"Unable to find the 'GESTALT/data' directory - does it exist?")
+
+        experimentsDirectoryPath = os.path.join(dataDirectory, "experiments")
+        assert (os.path.exists(experimentsDirectoryPath),"'data/experiments' does not exist.")
+
+        experimentDirectoryPath = os.path.join(experimentsDirectoryPath, experiment_name)
+        
+        if os.path.exists(experimentDirectoryPath) == False:
+            os.mkdir(experimentDirectoryPath)
+        
+        assert (os.path.exists(experimentDirectoryPath),"'data/experiments/'"+experiment_name+" does not exist.")
+
+        typeDirectoryPath = os.path.join(experimentDirectoryPath, saveType)
+        
+        if os.path.exists(typeDirectoryPath) == False:
+            os.mkdir(typeDirectoryPath)
+        
+        assert (os.path.exists(typeDirectoryPath),"'data/experiments/'"+experiment_name+saveType+" does not exist.")
+
+        filePathToSave = typeDirectoryPath+"/"+saveType+str(number)+".csv"
+
+        df_to_save = pd.DataFrame(data=data)
+
+        df_to_save.to_csv(filePathToSave,sep=",",header=True)
+
+        print("FILEPATHTOSAVE", filePathToSave)
+
+        return(filePathToSave)
--- code/utils/test_dataGenerator.py
+++ code/utils/test_dataGenerator.py
@@ -5,6 +5,7 @@
 import sys
 import os
 import unittest
+
 # Library Imports
 import pandas as pd
 import numpy as np
@@ -41,7 +42,73 @@ class test_compass_experiments(unittest.TestCase):
         pt = (0,0)
         points = [pt]
 
-        self.assertEqual(self.DG.generateMatrix(locationSize=0, numObjects=1, labels=['A']), points)
+        self.assertEqual(self.DG.generateMatrix(scaleFactor=0, edgeFactor=1), points)
+
+    def test_multi_nodes(self):
+        #Note: since it's probabalistic this might fail occasioanally
+        pt1 = (0,0)
+        pt2 = (0,1)
+        pt3 = (1,0)
+
+        points = [pt1,pt2,pt3]
+
+        generatedGraph = self.DG.generateMatrix(scaleFactor=1, edgeFactor=1)
+
+        self.assertListEqual(generatedGraph,points)
+
+
+    def test_labelNodes(self):
+
+        labelledNodes = {
+                        "name":["i"],
+                        "longitude":[0],
+                        "latitude":[0]
+                        }
+        
+        pt = (0,0)
+        points = [pt]
+        numClasses = 4
+
+        self.assertDictEqual(self.DG.labelNodes(points=points,numClasses=numClasses, random_seed=3), labelledNodes)
+
+
+    def test_multiLabelling(self):
+
+        labelledNodes = {
+                        "name":["i","i","I"],
+                        "longitude":[0,0,1],
+                        "latitude":[0,1,0]
+                        }
+
+        pt1 = (0,0)
+        pt2 = (0,1)
+        pt3 = (1,0)
+
+        points = [pt1,pt2,pt3]
+        numClasses = 4
+
+        self.assertDictEqual(self.DG.labelNodes(points=points,numClasses=numClasses, random_seed=3), labelledNodes)
+
+
+    def test_save_location(self):
+
+        experiment = "test1"
+        saveType = "location"
+        loc1 = "location_1"
+
+        loc1_labels = {
+                        "name":["i"],
+                        "longitude":[0],
+                        "latitude":[0]
+                        }
+        
+        saveLocation = self.DG.saveToFile(experiment_name=experiment, saveType=saveType, number=1, data=loc1_labels)
+
+        self.assertTrue(saveLocation.endswith("/data/experiments/test1/location/location1.csv"))
+        
+        self.assertTrue(os.path.isfile(saveLocation))
+
+
 
 
 # Main Function
