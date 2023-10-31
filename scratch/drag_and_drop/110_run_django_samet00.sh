ef60a354efa0f0cf5edc9d98b4ea0418541b4997
Nicole Schneider <nsch@samet00.umiacs.umd.edu>
2023-10-31 17:51:04 -0400

    django samet00 script now launches tmux

--- scratch/drag_and_drop/110_run_django_samet00.sh
+++ scratch/drag_and_drop/110_run_django_samet00.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "activating environment..."
-source ../../gestalt_env/bin/activate
+source ../../../gestalt_env/bin/activate
 
-echo "starting server..."
-python manage.py runserver 0.0.0.0:8000
+echo "starting tmux session..."
+tmux new-session -d -s gestalt python manage.py runserver 0.0.0.0:8000
