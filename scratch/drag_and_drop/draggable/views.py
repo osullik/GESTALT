e62fc97c1a62fe23a4bfa99c108aea28acf1ae27
nicoleschneider <nsch@umd.edu>
2023-11-08 16:10:23 -0500

    backend search functionality restored in new version of UI

--- scratch/drag_and_drop/draggable/views.py
+++ scratch/drag_and_drop/draggable/views.py
@@ -30,19 +30,45 @@ def get_regions(request):
     }
     return JsonResponse(response_data)
 
+def set_data_structure_paths(request):
+    dataDirectory = ""
+    for p in sys.path:
+        if p.endswith("media"):
+            dataDirectory = p 
+
+    assert (dataDirectory in sys.path),"Unable to find the 'GESTALT/data' directory"
+        
+    # Set data file paths based on region choice
+    if request.session['region'] == "Swan Valley, Australia":
+        print("Region set to SV.............")
+        request.session['CONCEPT_MAPS_PATH'] = os.path.join(dataDirectory,'data', 'SV', 'output', 'concept_mapping', 'ConceptMaps_DBSCAN_PredictedLocations_FT=0.0.pkl')
+        request.session['INVERTED_INDEX'] = os.path.join(dataDirectory,'data', 'SV', 'output', 'ownershipAssignment', 'DBSCAN_PredictedLocations_FT=0.0.csv')
+        request.session['LOCATION_STRUCTURE_PATH'] = os.path.join(dataDirectory,'data', 'SV', 'output', 'concept_mapping', 'RelativeLocations_DBSCAN_PredictedLocations_FT=0.0.JSON')
+    elif request.session['region'] == "Washington D.C.":
+        print("Region set to DC.............")
+        print("DC functionality not implemented yet......")  #TODO
+    else:
+        print("UNRECOGNIZED REGION SELECTED")
 
 def set_region(request):
     if request.method == 'POST':
         request.session['region'] = request.POST.get('name')
         print("Region selection has been set to.... ", request.session['region'])
 
+        set_data_structure_paths(request)
+
         response_data = {'success': True}
         return JsonResponse(response_data)
     
 
 def get_objects(request):
+    # Load data structures using file paths saved in request session
+    invertedIndex = InvertedIndex(request.session['INVERTED_INDEX'])
+    request.session['VOCAB'] = list(invertedIndex.ii.keys())
+    print('VOCAB is:', request.session['VOCAB'])
+
     response_data = {
-        'objects': ['tmp']#request.session['VOCAB']
+        'objects': request.session['VOCAB']
     }
     return JsonResponse(response_data)
 
@@ -51,12 +77,79 @@ def set_search_params(request):
         request.session['object_query'] = request.POST.get('object_query')
         request.session['search_type'] = request.POST.get('search_type')
         request.session['cardinality_invariant'] = (request.POST.get('knows_cardinality') == 'false')
-        response_data = {'success': True}
+        request.session['canvas_center'] = request.POST.get('canvas_center')
+
         print("Query was...", request.session['object_query'])
+        print("Canvas center is...", request.POST.get('canvas_center'))
+
+        response_data = {'success': True}
         return JsonResponse(response_data)
     
+def parse_query_from_dict(query_dict):
+    # Create query dataframe from everything in box_data  
+    flatDict = {"name":[], "longitude":[], "latitude":[], "predicted_location":[]}
+
+    for key in query_dict.keys():
+        flatDict["name"].append(query_dict[key]["name"])
+        flatDict["longitude"].append(query_dict[key]["x"])
+        flatDict["latitude"].append(query_dict[key]["y"])
+        flatDict["predicted_location"].append("PICTORIAL_QUERY")
+    
+    query_df = pd.DataFrame.from_dict(flatDict, orient='columns')
+    return query_df
+
+def obj_obj_search(request, query_df, card_invariance):
+    CM = ConceptMapper()
+    queriesDict = CM.createConceptMap(input_df=query_df, inputFile=None, cm_type='query')
+    for key in queriesDict.keys():
+        lonOrder, latOrder = queriesDict[key][1]
+        searchOrder = CM.getSearchOrder(lonOrder, latOrder)
+        print("SEARCHING FOR THIS searchOrder: ", searchOrder)
+
+        with open(request.session['CONCEPT_MAPS_PATH'], "rb") as inFile:
+            conceptMaps = pickle.load(inFile)
+    
+    results = []
+    for locationCM in conceptMaps.keys():
+        result = CM.searchMatrix(conceptMaps[locationCM], searchOrder.copy())
+        if result == True: 
+            results.append(locationCM)
+            result = False
+
+    return results
+
+def obj_loc_search(request, query_df, card_invariance):
+        #with open(request.session['LOCATION_STRUCTURE_PATH'], "r") as inFile:
+        #    referenceLocations = json.load(inFile)
+        # TODO
+    return None
+
+def search(request, query_df):
+    # Do the search  
+    print("SEARCH TYPE IS: ", request.session['search_type'])
+    print("CARDINALITY INVARIANT: ", request.session['cardinality_invariant'])
+
+    if request.session['search_type'] == "Object":
+        print("\n\n= = = = = = = = =  OBJECT CENTRIC SEARCH = = = = = = = = = \n")
+        results = obj_obj_search(request, query_df, card_invariance=request.session['cardinality_invariant'])
+    elif request.session['search_type'] == "Location":
+        print("\n\n= = = = = = = = =  LOCATION CENTRIC SEARCH = = = = = = = = = \n")
+        results = obj_loc_search(request, query_df, card_invariance=request.session['cardinality_invariant'])
+    else:
+        print("UNRECOGNIZED SEARCH TYPE")
+        results = None
+
+    return results
+    
 def get_search_result(request):
-    results = ['temp loc1','tmp loc2']
+    # Parse search params
+    query_dict = json.loads(request.session['object_query'])
+
+    query_df = parse_query_from_dict(query_dict)
+    print(query_df)
+
+    results = search(request, query_df)
+
 
     # Display results to server terminal for verification
     if not results or len(results) == 0:
