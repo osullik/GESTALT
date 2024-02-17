e87b7295a7afbeff13a7f587ef1fe14c4ccd2414
Nicole Schneider <nsch@umd.edu>
2024-02-17 13:15:57 -0500

    ADD: Point class angle/rotate methods and tests

--- code/src/canvas.py
+++ code/src/canvas.py
@@ -1,7 +1,10 @@
 import itertools
+import math
+import copy
+import numpy as np
 
 class Point():
-    def __init__(self, name:str, x_coord:int, y_coord:int, id:int):
+    def __init__(self, name:str, x_coord:int, y_coord:int, id:int=None):
         self._x = x_coord
         self._y = y_coord
         self._name = name
@@ -9,6 +12,9 @@ class Point():
 
     def __str__(self)->str:
         return self._name + "(" + str(self._x) + "," + str(self._y) + ")"
+    
+    def __eq__(self, other)->bool:
+        return isinstance(other, Point) and self._x == other.get_x() and self._y == other.get_y()
 
     def get_x(self) ->int:
         return self._x
@@ -22,7 +28,41 @@ class Point():
     def get_id(self)->int:
         return(self._id)
 
-    def __eq__(self, other)->bool:
-        return isinstance(other, Point) and self._x == other.get_x() and self._y == other.get_y()
+
+    # Assume angle is in degrees
+    def rotate(self, origin, angle):
+        angle_rad = math.radians(angle)
+
+        ox, oy = (origin.get_x(), origin.get_y())
+        px, py = (self._x, self._y)
+
+        qx = ox + math.cos(angle_rad) * (px - ox) - math.sin(angle_rad) * (py - oy)
+        qy = oy + math.sin(angle_rad) * (px - ox) + math.cos(angle_rad) * (py - oy)
+
+        self._x, self._y = qx, qy
+
+
+    def translate(self, xshift, yshift):
+        self._x += xshift
+        self._y += yshift
+
+    
+    def get_angle(self, origin, other):
+        a = np.array([self._x, self._y])
+        b = np.array([origin.get_x(), origin.get_y()])
+        c = np.array([other.get_x(), other.get_y()])
+
+        ba = a - b
+        bc = c - b
+
+        cosine_angle = np.dot(ba, bc) / (np.linalg.norm(ba) * np.linalg.norm(bc))
+        angle = np.degrees(np.arccos(cosine_angle))
+
+        if self._x < origin.get_x():
+            angle = 360 - angle
+
+        return angle
+
+
 
 
--- code/src/test_canvas.py
+++ code/src/test_canvas.py
@@ -1,19 +1,93 @@
 import pytest
 import math
+from copy import copy
 
 from canvas import Point
 
 class TestPoint:
     @pytest.fixture
-    def setup_point(self):
+    def setup_0_0(self):
         test_point = Point(name="testPoint", x_coord=0, y_coord=0, id=42)
         yield test_point
         del test_point
 
-    def test_get_coords(self, setup_point):
-        coords = (0,0)
-        assert setup_point.get_x() == coords[0] and setup_point.get_y() == coords[1]
+    @pytest.fixture
+    def setup_5_0(self):
+        test_point = Point(name="testPoint", x_coord=5, y_coord=0, id=42)
+        yield test_point
+        del test_point
 
-    def test_equals(self, setup_point):
-        assert setup_point == Point(name="anotherTestPoint", x_coord=0, y_coord=0, id=13)
+    @pytest.fixture
+    def setup_5_10(self):
+        test_point = Point(name="testPoint", x_coord=5, y_coord=10, id=42)
+        yield test_point
+        del test_point
+
+
+    def test_get_coords(self, setup_0_0):
+        coords = (0,0)
+        assert setup_0_0.get_x() == coords[0] and setup_0_0.get_y() == coords[1]
+
+    def test_equals(self, setup_0_0):
+        assert setup_0_0 == Point(name="anotherTestPoint", x_coord=0, y_coord=0, id=13)
+
+    def test_rotate1(self, setup_0_0):
+        setup_0_0.rotate(origin=Point('C', 5, 5), angle=225)
+        answer = Point("testPointPrime", 5, 5+5*math.sqrt(2))
+        assert pytest.approx(setup_0_0.get_x(), 0.0001) == answer.get_x()
+        assert pytest.approx(setup_0_0.get_y(), 0.0001) == answer.get_y()
+
+    def test_rotate2(self, setup_5_10):
+        setup_5_10.rotate(origin=Point('C', 5, 5), angle=180)
+        answer = Point("testPointPrime", 5, 0)
+        assert pytest.approx(setup_5_10.get_x(), 0.0001) == answer.get_x()
+        assert pytest.approx(setup_5_10.get_y(), 0.0001) == answer.get_y()
+
+    def test_translate(self, setup_0_0):
+        setup_0_0.translate(20, -15)
+        assert setup_0_0 == Point("shiftedPoint", 20, -15)
+
+    def test_angle1(self, setup_5_0):
+    #     #
+    #     # 5     N
+    #     # 4    |z\
+    #     # 3    |  \   c
+    #     # 2  p |   \
+    #     # 1    |x  y\
+    #     # 0     C --- P
+    #     #         n
+    #     #      012345
+        C = Point('centroid',0,0)
+        N = Point('north',0,5)
+        P = setup_5_0
+        x, y, z = 90, 45, 45
+
+        assert pytest.approx(P.get_angle(origin=C, other=N), 0.0001) == x
+        # assert pytest.approx(N.get_angle(origin=P, other=C), 0.0001) == y
+        # assert pytest.approx(C.get_angle(origin=N, other=P), 0.0001) == z
+
+        # assert pytest.approx(N.get_angle(origin=C, other=P), 0.0001) == 360 - x
+        # assert pytest.approx(C.get_angle(origin=P, other=N), 0.0001) == 360 - y
+        # assert pytest.approx(P.get_angle(origin=N, other=C), 0.0001) == 360 - z
+
+
+    def test_angle2(self, setup_0_0):
+    #     # Test we get external angle of rotation when required
+    #     #
+    #     # 5           N
+    #     # 4         / |
+    #     # 3        /  |
+    #     # 2     c /   | p
+    #     # 1      /    |
+    #     # 0     P --- C
+    #     #          n   (x)
+    #     #        012345
+        
+        C = Point('centroid2',5,0)
+        N = Point('north2',5,5)
+        P = setup_0_0
+        x = 270
+
+        assert pytest.approx(P.get_angle(origin=C, other=N), 0.0001) == x
+        # assert pytest.approx(N.get_angle(origin=C, other=P), 0.0001) == 360 - x
 
