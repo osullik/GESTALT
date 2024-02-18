97ff297d1412dfa4f99c1f7ad82097088c2e03d5
Nicole Schneider <nsch@umd.edu>
2024-02-17 21:04:37 -0500

    ADD: more complex tests for COMPASS_OO get_search_order

--- code/src/test_compass.py
+++ code/src/test_compass.py
@@ -11,10 +11,22 @@ class TestConceptMap:
 class TestCOMPASS_OO_Search:
     @pytest.fixture
     def setup_COMPASS_OO_Search(self):
-        test_searcher = COMPASS_OO_Search(None)
+        test_searcher = COMPASS_OO_Search(None)  # TODO: make this an actual data value
         yield test_searcher
         del test_searcher
 
-    def test_search_order(self, setup_COMPASS_OO_Search):
+    def test_search_order1(self, setup_COMPASS_OO_Search):
         latlist, longlist = ["A"], ["A"]
         assert setup_COMPASS_OO_Search.get_search_order(longlist, latlist) == ["A"]
+
+    def test_search_order2(self, setup_COMPASS_OO_Search):
+        latlist, longlist = ["A","B"], ["A","B"]
+        assert setup_COMPASS_OO_Search.get_search_order(longlist, latlist) == ["A","B"]
+
+    def test_search_order3(self, setup_COMPASS_OO_Search):
+        latlist, longlist = ["A","B"], ["B","A"]
+        assert setup_COMPASS_OO_Search.get_search_order(longlist, latlist) == ["A","B"]
+
+    def test_search_order4(self, setup_COMPASS_OO_Search):
+        latlist, longlist = ["A","B","C"], ["C","B","A"]
+        assert setup_COMPASS_OO_Search.get_search_order(longlist, latlist) == ["A","C","B"]
